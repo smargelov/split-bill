af2ecbd471db3c6f84a5a5a3fd3c1112ac1d31d4
Sergey Margelov <smargelov@gmail.com>
2024-08-13 00:07:57 +0400

    Add PWA settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'url'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
 	resolve: {
@@ -11,6 +12,47 @@ export default defineConfig({
 	},
 	plugins: [
 		vue(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'Split Bill',
+				short_name: 'Split Bill',
+				description: 'Split bill between friends',
+				theme_color: '#232323',
+				icons: [
+					{
+						src: '/favicon-32x32.png',
+						sizes: '32x32',
+						type: 'image/png',
+						purpose: 'any',
+					},
+					{
+						src: '/pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'any',
+					},
+					{
+						src: '/pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any',
+					},
+					{
+						src: '/pwa-maskable-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+					{
+						src: '/pwa-maskable-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+				],
+			},
+		}),
 		AutoImport({
 			imports: ['vue', '@vueuse/core'],
 			resolvers: [ElementPlusResolver()],
